f27535c891b2b472855280342ef29f82999ee35f
jesuspallarees <jespaldia@gmail.com>
2026-02-06 00:27:59 +0100

    deleted problematic files

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <script setup lang="ts">
-import Background from './components/Background.vue';
-import Header from './components/Header.vue';
-import Linktree from '@/views/Linktree.vue';
+import Hero from '../components/Hero.vue';
+import LinkCard from '../components/LinkCard.vue';
+import { Linkedin, Github } from 'lucide-vue-next';
+
+const myLinks = [
+    {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/jesuspallares/',
+        icon: Linkedin
+    },
+    {
+        name: 'Github',
+        url: 'https://www.github.com/jesuspallarees/',
+        icon: Github
+    },
+]
 </script>
 
 <template>
-  <Background />
-  <Header />
-  <Linktree />
+    <div class="main-container">
+        <Hero />
+        <LinkCard v-for="link in myLinks" :key="link.name" v-bind="link" />
+    </div>
 </template>
+
+<style scoped>
+.main-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    transition: background-color 0.5s ease;
+}
+
+:global(html.dark) .main-container {
+    background-color: #000000;
+}
+</style>
